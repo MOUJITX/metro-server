9568a1ffd7055be003e3ee5c1f9eedcb1005ab75
moujitx <moujitx_chn@qq.com>
2025-08-24 21:48:50 +0800

    Update import

--- src/main/java/com/moujitx/metro/server/entity/Station.java
+++ src/main/java/com/moujitx/metro/server/entity/Station.java
@@ -1,13 +1,12 @@
 package com.moujitx.metro.server.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import java.util.Date;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+
+import java.util.Date;
 
 /**
  * @TableName station
